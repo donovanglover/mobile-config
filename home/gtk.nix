e2faa68b54365a02c9189dce3ff0c2322f48ddbc
Donovan Glover <donovan@dglover.co>
2025-02-21 19:23:21 -0500

    fix: don't duplicate gtk options from nix-config

--- home/gtk.nix
+++ home/gtk.nix
@@ -13,24 +13,6 @@ let
   };
 in
 {
-  gtk = {
-    enable = true;
-
-    gtk3.extraConfig = {
-      gtk-decoration-layout = "menu:";
-      gtk-xft-antialias = 1;
-      gtk-xft-hinting = 1;
-      gtk-xft-hintstyle = "hintfull";
-      gtk-xft-rgba = "rgb";
-      gtk-recent-files-enabled = false;
-    };
-
-    iconTheme = {
-      package = pkgs.fluent-icon-theme;
-      name = "Fluent-dark";
-    };
-  };
-
   stylix.targets.gtk.extraCss = # css
     ''
       phosh-lockscreen {
